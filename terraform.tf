6c2b1b3e04ab1059a8a7eadd7a45081fe7591eb6
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-02-12 08:18:58 -0500

    Dev (#46)
    * testing an update
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * removing commit
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * adding shas
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * creating workspace in project
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * updating to cloudflare v5
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * adding bucket creation
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "cloudflare" {
-  api_token = "${var.cloudflare_token}"
+  api_token = var.cloudflare_token
 }
