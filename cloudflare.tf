7fbdae79cc708326540546bca285514dbaca5675
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2026-02-12 07:42:40 -0500

    updating to cloudflare v5
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,16 +1,14 @@
-resource "cloudflare_workers_domain" "project_domain" {
+resource "cloudflare_workers_custom_domain" "project_domain" {
   account_id = var.cloudflare_account_id
   hostname   = "${var.project_name}.${var.environment}.${var.domain}"
-  service    = "${var.project_name}-${var.environment}"
+  service    = cloudflare_workers_script.project_script.script_name
   zone_id    = var.cloudflare_zone_id
-
-  depends_on = [cloudflare_workers_script.project_script]
 }
 
 resource "cloudflare_workers_route" "project_route" {
-  zone_id     = var.cloudflare_zone_id
-  pattern     = "${var.project_name}.${var.environment}.${var.domain}/*"
-  script_name = cloudflare_workers_script.project_script.name
+  zone_id  = var.cloudflare_zone_id
+  pattern  = "${var.project_name}.${var.environment}.${var.domain}/*"
+  script   = cloudflare_workers_script.project_script.script_name
 }
 
 resource "null_resource" "project_id" {
@@ -33,70 +31,73 @@ data "local_file" "load_project_id" {
 
 resource "cloudflare_workers_script" "project_script" {
   account_id         = var.cloudflare_account_id
-  name               = "${var.project_name}-${var.environment}"
-  content            = file("${path.module}/dist/index.mjs")
+  script_name        = "${var.project_name}-${var.environment}"
+  content_file       = "${path.module}/dist/index.mjs"
+  content_sha256     = filesha256("${path.module}/dist/index.mjs")
   compatibility_date = "2023-08-28"
-  module             = true
-
-  plain_text_binding {
-    name = "CORS_DOMAINS"
-    text = var.ALLOWED_HOSTS
-  }
-
-  plain_text_binding {
-    name = "ENVIRONMENT"
-    text = var.environment
-  }
-
-  plain_text_binding {
-    name = "GCP_LOGGING_PROJECT_ID"
-    text = var.GCP_LOGGING_PROJECT_ID
-  }
-
-  plain_text_binding {
-    name = "LOG_NAME"
-    text = "${var.project_name}_${var.environment}_worker_log"
-  }
-
-  r2_bucket_binding {
-    name        = "SCHEMAS_BUCKET"
-    bucket_name = "schemas-pulsedb-${var.environment}"
-  }
-
-  plain_text_binding {
-    name = "PULSE_DATASET"
-    text = "pulsedb_dataset"
-  }
-
-  plain_text_binding {
-    name = "PULSE_DATABASE_PROJECT_ID"
-    text = data.local_file.load_project_id.content
-  }
-
-  plain_text_binding {
-    name = "VERSION"
-    text = var.VERSION
-  }
-
-  secret_text_binding {
-    name = "GCP_LOGGING_CREDENTIALS"
-    text = var.GCP_LOGGING_CREDENTIALS
-  }
-
-  secret_text_binding {
-    name = "GCP_BIGQUERY_CREDENTIALS"
-    text = var.GCP_BIGQUERY_CREDENTIALS
-  }
-
-  secret_text_binding {
-    name = "GCP_USERINFO_CREDENTIALS"
-    text = var.GCP_USERINFO_CREDENTIALS
-  }
-
-  secret_text_binding {
-    name = "GLOBAL_SHARED_SECRET"
-    text = var.GLOBAL_SHARED_SECRET
-  }
+  main_module        = "index.mjs"
+  bindings = [
+    {
+      name = "CORS_DOMAINS"
+      type = "plain_text"
+      text = var.ALLOWED_HOSTS
+    },
+    {
+      name = "ENVIRONMENT"
+      type = "plain_text"
+      text = var.environment
+    },
+    {
+      name = "GCP_LOGGING_PROJECT_ID"
+      type = "plain_text"
+      text = var.GCP_LOGGING_PROJECT_ID
+    },
+    {
+      name = "LOG_NAME"
+      type = "plain_text"
+      text = "${var.project_name}_${var.environment}_worker_log"
+    },
+    {
+      name        = "SCHEMAS_BUCKET"
+      type        = "r2_bucket"
+      bucket_name = "schemas-pulsedb-${var.environment}"
+    },
+    {
+      name = "PULSE_DATASET"
+      type = "plain_text"
+      text = "pulsedb_dataset"
+    },
+    {
+      name = "PULSE_DATABASE_PROJECT_ID"
+      type = "plain_text"
+      text = data.local_file.load_project_id.content
+    },
+    {
+      name = "VERSION"
+      type = "plain_text"
+      text = var.VERSION
+    },
+    {
+      name = "GCP_LOGGING_CREDENTIALS"
+      type = "secret_text"
+      text = var.GCP_LOGGING_CREDENTIALS
+    },
+    {
+      name = "GCP_BIGQUERY_CREDENTIALS"
+      type = "secret_text"
+      text = var.GCP_BIGQUERY_CREDENTIALS
+    },
+    {
+      name = "GCP_USERINFO_CREDENTIALS"
+      type = "secret_text"
+      text = var.GCP_USERINFO_CREDENTIALS
+    },
+    {
+      name = "GLOBAL_SHARED_SECRET"
+      type = "secret_text"
+      text = var.GLOBAL_SHARED_SECRET
+    }
+  ]
 
   depends_on = [ data.local_file.load_project_id ]
 }
